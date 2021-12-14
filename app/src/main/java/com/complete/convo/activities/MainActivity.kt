81ec8edb12e0b5fb58dc8d5d33ce01d09a12e3c8
ritik <malritik@gmail.com>
2021-12-15 04:33:33 +0530

    Initial build

--- app/src/main/java/com/complete/convo/activities/MainActivity.kt
+++ app/src/main/java/com/complete/convo/activities/MainActivity.kt
@@ -275,4 +275,11 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onBackPressed() {
+        val a = Intent(Intent.ACTION_MAIN)
+        a.addCategory(Intent.CATEGORY_HOME)
+        a.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(a)
+    }
 }
